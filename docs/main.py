4456b4d7a3f0b8653450cb8d4fa4ae035f017541
cesar <cafiguer19@gmail.com>
2025-05-17 17:52:07 -0500

    display html str

--- docs/main.py
+++ docs/main.py
@@ -3,4 +3,6 @@ from datetime import datetime
 
 now = datetime.now()
 display(now.strftime("%m/%d/%Y, %H:%M:%S"))
-display("where will this display?", target="resultdiv")
+html_str = "<b>where will this display?</b>"
+display(html_str, target="resultdiv")
+
