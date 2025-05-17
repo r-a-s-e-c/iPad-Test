5e3f2469a33c3e55ff2d4f6d8251856bc1d717f8
cesar <cafiguer19@gmail.com>
2025-05-17 18:39:47 -0500

    fixed args

--- docs/main.py
+++ docs/main.py
@@ -5,5 +5,5 @@ now = datetime.now()
 display(now.strftime("%m/%d/%Y, %H:%M:%S"))
 html_str = "<b>where will this display?</b>"
 
-def print_message():
+def print_message(*args):
     display(html_str, target="resultdiv")
