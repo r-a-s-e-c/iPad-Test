3bf630ca409d25830e969c46bb82a380781ee544
cesar <cafiguer19@gmail.com>
2025-05-17 18:35:20 -0500

    adding button

--- docs/main.py
+++ docs/main.py
@@ -4,5 +4,6 @@ from datetime import datetime
 now = datetime.now()
 display(now.strftime("%m/%d/%Y, %H:%M:%S"))
 html_str = "<b>where will this display?</b>"
-display(html_str, target="resultdiv")
 
+def print_message():
+    display(html_str, target="resultdiv")
